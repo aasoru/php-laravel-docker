e453c0dc0fb18091cc2a99bfde7d8373faee5671
Aarón Sosa <aasoru@gmail.com>
2022-09-12 23:22:00 +0100

    test api routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    return response()->json([
+        'name' => env('APP_NAME'),
+        'vers.' => env('APP_VERSION'),
+
+    ]);
 });
